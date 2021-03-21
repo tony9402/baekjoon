8efa93a53c5ffb2ad576980c676ae1c42187a0c3
tony9402 <tony9402@naver.com>
2021-03-21 14:19:14 +0900

    [ADD] 1212 Solution

---
+++ solution/implementation/1212/main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string convert(char s, bool fillzero=false){
+    int n = s - 48;
+    string ret = "";
+    while(n){
+        ret += (n % 2) + '0';
+        n /= 2;
+    }
+    while(fillzero && (int)ret.size() < 3) ret += '0';
+    reverse(ret.begin(), ret.end());
+    return ret;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;cin >> s;
+    if(s == "0") {
+        cout << 0;
+        return 0;
+    }
+    string ans="";
+    for(int i=0;i<(int)s.size();i++){
+        ans += convert(s[i], i != 0);
+    }
+    cout << ans;
+
+    return 0;
+}
+
