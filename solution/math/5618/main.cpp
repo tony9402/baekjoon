a983012f2d6120a78a3f1874ca0330c29ab6eab1
tony9402 <tony9402@naver.com>
2021-03-31 13:01:56 +0900

    [ADD] 5618 Solution

---
+++ solution/math/5618/main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    int a, b; cin >> a >> b;
+    a = gcd(a, b);
+    if(n == 3) {
+        int c; cin >> c;
+        a = gcd(a, c);
+    }
+
+    vector<int> ans;
+    for(int i=1;i*i<=a;i++){
+        if(a % i != 0) continue;
+        ans.push_back(i);
+        if(i * i != a) ans.push_back(a / i);
+    }
+    sort(ans.begin(), ans.end());
+    for(int i=0;i<(int)ans.size();i++){
+        cout << ans[i] << '\n';
+    }
+
+    return 0;
+}
