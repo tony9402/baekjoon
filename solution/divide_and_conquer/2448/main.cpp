a734ba781a22ca48dec486f80785962f151a2f69
tony9402 <tony9402@naver.com>
2021-04-04 21:39:33 +0900

    [ADD] Add 2448 solution

---
+++ solution/divide_and_conquer/2448/main.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+char stars[3500][6500];
+char DB[3][6] = {
+    "  *  ",
+    " * * ",
+    "*****"
+};
+
+void solve(int y, int x, int s) {
+    if(s == 1) {
+        for(int i=0;i<3;i++){
+            for(int j=0;j<5;j++){
+                stars[y + i][x + j] = DB[i][j];
+            }
+        }
+        return;
+    }
+    s /= 2;
+    solve(y, x + 3 * s, s);
+    solve(y + 3 * s, x, s);
+    solve(y + 3 * s, x + 6 * s, s);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    solve(0, 0, n / 3);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<2*n-1;j++){
+            if(stars[i][j] == '*') cout << '*';
+            else cout << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
