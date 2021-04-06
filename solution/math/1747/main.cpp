9e45b23ded77c839b91a3f52e620137eb7c101e5
tony9402 <tony9402@naver.com>
2021-04-06 14:14:46 +0900

    [ADD] add Math Solution

---
+++ solution/math/1747/main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isPrime(int x) {
+    if(x <= 1) return false;
+    for(int i=2;i*i<=x;i++){
+        if(x % i == 0) return false;
+    }
+    return true;
+}
+
+bool isPalindrome(int x) {
+    int origin = x;
+    int rev    = 0;
+    while(x) {
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+    return origin == rev;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    while(true) {
+        if(isPalindrome(n) && isPrime(n)) break;
+        n ++;
+    }
+    cout << n;
+
+    return 0;
+}
