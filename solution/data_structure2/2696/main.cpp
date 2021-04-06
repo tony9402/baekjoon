1274316d1f526d062a8fda44b139b5715cc46ace
tony9402 <tony9402@naver.com>
2021-04-06 13:49:50 +0900

    [ADD] add Data Structure2 Solutions

---
+++ solution/data_structure2/2696/main.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        priority_queue<int> L;
+        priority_queue<int, vector<int>, greater<int>> R;
+        vector<int> answer;
+        for(int i=0;i<n;i++) {
+            int x; cin >> x;
+            R.push(x);
+
+            while(L.size() < R.size()) {
+                L.push(R.top());
+                R.pop();
+            }
+            while(L.size() > R.size() + 1) {
+                R.push(L.top());
+                L.pop();
+            }
+            while(!L.empty() || !R.empty()) {
+                int valueL = L.top();
+                int valueR = R.top();
+                if(valueL <= valueR) break;
+                L.pop();
+                R.pop();
+                L.push(valueR);
+                R.push(valueL);
+            }
+            
+            if(i % 2 == 0) answer.push_back(L.top());
+        }
+        cout << answer.size() << '\n';
+        for(int i=0;i<(int)answer.size();i++) {
+            cout << answer[i];
+            if(i + 1 != (int)answer.size() && (i + 1) % 10 == 0) cout << '\n';
+            else cout << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
---
+++ solution/data_structure2/7662/main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+        map<int, int> mp;
+        int n; cin >> n;
+        for(int i=0;i<n;i++) {
+            char cmd; cin >> cmd;
+            int x;cin >> x;
+            
+            if(cmd == 'I') {
+                mp[x] += 1;
+            }
+            else {
+                if(mp.empty()) continue;
+                if(x == -1) {
+                    if(mp.begin()->second == 1) {
+                        mp.erase(mp.begin()->first);
+                    }
+                    else {
+                        mp.begin()->second --;
+                    } 
+                }
+                else {
+                    if(mp.rbegin()->second == 1) {
+                        mp.erase(mp.rbegin()->first);
+                    }
+                    else {
+                        mp.rbegin()->second --;
+                    }
+                }
+            }
+        }
+        if(mp.empty()) cout << "EMPTY\n";
+        else cout << mp.rbegin()->first << ' ' << mp.begin()->first << '\n';
+    }
+
+    return 0;
+}
