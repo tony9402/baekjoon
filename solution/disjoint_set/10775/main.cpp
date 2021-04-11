12ce94c1608c57458e43a0880f0f08a93d6f3fd9
tony9402 <tony9402@naver.com>
2021-04-11 23:53:36 +0900

    [UPD] add disjoint solution

---
+++ solution/disjoint_set/10775/main.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+constexpr int MAXN = 100005;
+int uf[MAXN];
+
+int find(int x) {
+    if(uf[x] < 0) return x;
+    return uf[x] = find(uf[x]);
+}
+
+bool merge(int a, int b){
+    a = find(a);
+    b = find(b);
+    if(a == b)return false;
+    uf[b] = a;
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M; cin >> N >> M;
+    for(int i=0;i<=N;i++) uf[i] = -1;
+    int answer = 0;
+    for(int i=0;i<M;i++){
+        int x;cin >> x;
+        int cur = find(x);
+        if(cur == 0) break;
+        merge(cur - 1, cur);
+        answer ++;
+    }
+    cout << answer;
+
+    return 0;
+}
---
+++ solution/disjoint_set/4195/main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+constexpr int MAXN = 200005;
+int uf[MAXN], siz[MAXN];
+
+int find(int x) {
+    if(uf[x] < 0) return x;
+    return uf[x] = find(uf[x]);
+}
+
+bool merge(int a, int b) {
+    a = find(a); b = find(b);
+    if(a == b)return false;
+    uf[b] = a;
+    siz[a] += siz[b];
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--){
+        for(int i=0;i<MAXN;i++) {
+            uf[i] = -1;
+            siz[i] = 1;
+        }
+        int n; cin >> n;
+        unordered_map<string, int> mp;
+        int idx = 0;
+        
+        for(int i=0;i<n;i++){
+            string a, b; cin >> a >> b;
+            if(mp.count(a) == 0) mp[a] = idx++;
+            if(mp.count(b) == 0) mp[b] = idx++;
+            merge(mp[a], mp[b]);
+            cout << siz[find(mp[a])] << '\n';
+        }
+    }
+
+    return 0;
+}
