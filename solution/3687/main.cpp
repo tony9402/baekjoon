6a5dc847f4be05f6961f53accce075ea63812a1f
tony9402 <tony9402@naver.com>
2021-04-11 23:26:51 +0900

    [UPD] 쩜's 추천 문제 성냥개비 풀이

---
+++ solution/3687/main.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+// 6, 2, 5, 5, 4, 5, 6, 3, 7, 6
+const string DB[8] = { "0", "0", "1", "7", "4", "2", "0", "8" };
+string minstr[105] = { "0", "0", "1", "7", "4", "2", "6", "8", "10" };
+string maxstr[105];
+
+string getMax(int cnt) {
+    string ans;
+    while(cnt) {
+        if(cnt & 1) {
+            ans += '7';
+            cnt -= 3;
+        }
+        else {
+            ans += '1';
+            cnt -= 2;
+        }
+    }
+    return ans;
+}
+
+string getMin(const string &a, const string &b) {
+    if(a.size() > b.size()) return b;
+    if(a.size() < b.size()) return a;
+    for(int i=0;i<(int)a.size();i++){
+        if(a[i] < b[i]) return a;
+        if(a[i] > b[i]) return b;
+    }
+    return a;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    for(int i=2;i<=100;i++)maxstr[i] = getMax(i);
+    for(int i=9;i<=100;i++){
+        minstr[i] = minstr[i - 2] + DB[2];
+        for(int j=3;j<=7;j++) minstr[i] = getMin(minstr[i], minstr[i - j] + DB[j]);
+    }
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        cout << minstr[n] << " " << maxstr[n] << '\n';
+    }
+
+    return 0;
+}
