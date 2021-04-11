5a1ae217a7aaad226fcfab6b161ff0cb3f571ea2
tony9402 <tony9402@naver.com>
2021-04-12 01:00:29 +0900

    [UPD] add MST solutions

---
+++ solution/minimum_spanning_tree/1647/main.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+constexpr int MAXN = 100005;
+int uf[MAXN];
+
+int find(int x) {
+    if(uf[x] < 0)return x;
+    return uf[x] = find(uf[x]);
+}
+
+bool merge(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if(a == b)return false;
+    uf[b] = a;
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    vector<tuple<int, int, int>> w;
+    for(int i=0;i<MAXN;i++) uf[i] = -1;
+    
+    int N, M; cin >> N >> M;
+    for(int i=0;i<M;i++) {
+        int a, b, c; cin >> a >> b >> c;
+        w.emplace_back(c, a, b);
+    }
+    sort(w.begin(), w.end());
+    
+    ll ans = 0;
+    int cnt = 0;
+    for(auto &[c, a, b]: w) {
+        if(merge(a, b)) {
+            ans += c;
+            if(++cnt == N - 2) break;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
+
---
+++ solution/minimum_spanning_tree/1922/main.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+constexpr int MAXN = 1005;
+int uf[MAXN];
+
+int find(int x) {
+    if(uf[x] < 0)return x;
+    return uf[x] = find(uf[x]);
+}
+
+bool merge(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if(a == b)return false;
+    uf[b] = a;
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    vector<tuple<int, int, int>> w;
+    for(int i=0;i<MAXN;i++) uf[i] = -1;
+    
+    int N, M; cin >> N >> M;
+    for(int i=0;i<M;i++) {
+        int a, b, c; cin >> a >> b >> c;
+        w.emplace_back(c, a, b);
+    }
+    sort(w.begin(), w.end());
+    
+    int ans = 0;
+    for(auto &[c, a, b]: w) {
+        if(merge(a, b)) ans += c;
+    }
+    cout << ans;
+
+    return 0;
+}
+
