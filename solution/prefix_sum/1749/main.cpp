01e0fadfca7c4e14d47e2b06c97a9e277c5467f4
tony9402 <tony9402@naver.com>
2021-04-18 05:52:55 +0900

    [ADD] prefix sum solution

---
+++ solution/prefix_sum/1749/main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int arr[222][222];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M; cin >> N >> M;
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=M;j++) {
+            cin >> arr[i][j];
+            arr[i][j] += arr[i-1][j];
+        }
+    }
+
+    int answer = INT_MIN;
+    for(int i=1;i<=N;i++){
+        for(int j=i;j<=N;j++) {
+            int sum = 0;
+            for(int k=1;k<=M;k++){
+                int cur = arr[j][k] - arr[i - 1][k];
+                sum += cur;
+                if(sum < cur) sum = cur;
+                answer = max(answer, sum);
+            }
+        }
+    }
+    cout << answer;
+
+    return 0;
+}
