c3b771edb0ca198c895caa02e8cf1eb5f7d62544
tony9402 <tony9402@naver.com>
2021-04-22 22:30:07 +0900

    [UPD] 중간 저장

--- scripts/API.py
+++ scripts/API.py
@@ -2,11 +2,12 @@ from urllib import request
 import ssl
 import json
 import atexit
+import time
 
 # ProblemId: 문제 번호
 # ProblemName   : 문제 이름
 # ProblemTier   : 문제 난이도 (SolvedAC)
-# lastupdate    : 업데이트 로그 (ex 1618947707.736831)
+# lastupdate    : 업데이트 로그 (ex 1618947707.736831 [minute])
 
 # Database + request
 class SolvedAPI:
@@ -16,29 +17,105 @@ class SolvedAPI:
         self.config = config
         self.database = dict()
 
+        # Load Database
         self.__load_database()
+
+        # For saving Database
         atexit.register(self.__save_database)
 
     def __load_database(self):
-        file = self.config.get("database")
-        assert type(file) == str
-        with open(file, 'r') as f:
+        option = "database"
+        File = self.config.get(option)
+
+        # Must Be str (string)
+        assert type(File) == str, f"[*** Config Error] Not Found {option}, Check config.json"
+
+        with open(File, 'r') as f:
             self.database = json.load(f)
             f.close()
 
     def __save_database(self):
-        # file = self.config.get("database")
-        file = self.config.get('testdatabase')
-        assert type(file) == str
-        with open(file, 'w') as f:
+        option = "testdatabase"
+        File = self.config.get(option)
+
+        # Must Be str (string)
+        assert type(File) == str, f"[*** Config Error] Not Found {option}, Check config.json"
+
+        with open(File, 'w') as f:
             f.write(json.dumps(self.database, indent=4, ensure_ascii=False))
             f.close()
 
-    def request(self):
-        pass
+    # using SolvedAPI
+    def __request(self, problemId):
+        URL      = f"{self.config['targeturl']}/{self.config['route']['problemid']}{problemId}"
+        response = request.urlopen(request.Request(URL), context=self.ssl_context)
+        JSON     = json.loads(response.read().decode(self.config.get('encoding')))
 
-    def getProblemName(self, problemId):
-        pass
+        assert JSON.get("success") == True, "[*** API Error] Failed"
+        INFO     = JSON["result"]["problems"][0]
+
+        data = {
+            "problemId": INFO.get('id'),
+            "problemLevel": INFO.get('level'),
+            "problemName": INFO.get('title'),
+            "average_try": INFO.get('average_try'),
+            "solvedtags": list()
+        }
+
+        for tag in INFO.get('tags', [ ]):
+            solvedtags = dict()
+            for config_tags in self.config.get('solvedtags'):
+                solvedtags[ config_tags ] = tag[ config_tags ]
+            data['solvedtags'].append(solvedtags)
+
+        return data
+
+    # update
+    def request(self, problemId):
+
+        # Check problemId Type
+        assert type(problemId) == str, f"[*** Type Error] problemId type is {type(problemId)}.\n It must be string"
+
+        # information : (in database)
+        exist, information = self.getProblemInformation(problemId)
+        if exist:
+            lastupdate = information.get('lastupdate', 0.0)
+
+            assert type(lastupdate) == float, "[*** Type Error] lastupdate type is {type(lastupdate)}.\n It must be float"
+            if lastupdate + self.config.get('update') < time.time():
+                return information
+
+        # update
+        return self.__request(problemId)
+
+    # update force
+    def requestForce(self, problemId):
+
+        # Check ProblemId Type
+        assert type(problemId) == str, f"[*** Type Error] problemId type is {type(ProblemId)}.\n It must be string"
+        return self.__requeset(problemId)
+
+    def getProblemInformation(self, problemId):
+
+        # Check Database
+        assert type(problemId) == str, f"[*** Type Error] problemId type is {type(problemId)}.\n It must be string"
+
+        if not problemId in self.database.keys():
+            return False, dict()
+        
+        return True, self.database.get(problemId)
+
+    def saveInformation(self, data):
+
+        # Check data type
+        assert type(data) == dict, f"[*** Type Error] data type is {type(data)}.\n It must be dict"
+
+        data['lastupdate'] = time.time()
+        problemId = data.get('problemId')
+        print(problemId, self.database)
+        if problemId in self.database.keys():
+            del self.database[problemId]
+        self.database.update({problemId: data})
 
 if __name__=="__main__":
     config = dict()
@@ -47,4 +124,5 @@ if __name__=="__main__":
         config = json.load(f)
         f.close()
     api = SolvedAPI(config.get("API"))
-    print(dir(api))
+    data = api.request('1001')
+    api.saveInformation(data)
