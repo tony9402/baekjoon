f650acbefd61ecfabdeabfa0d146eb7cba28e81f
tony9402 <tony9402@naver.com>
2021-04-25 04:46:00 +0900

    [ADD] 15650 solution

---
+++ solution/backtracking/15650/main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int choose[10], N, M;
+bool used[10];
+
+void dfs(int pre, int cnt) {
+    if(cnt == M) {
+        for(int i=0;i<M;i++) {
+            cout << choose[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=pre + 1;i<=N;i++) {
+        if(used[i]) continue;
+        used[i] = true;
+        choose[cnt] = i;
+        dfs(i, cnt + 1);
+        used[i] = false;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N >> M;
+    dfs(0, 0);
+
+    return 0;
+}
+
