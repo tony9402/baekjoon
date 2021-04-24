966fc7388ae2839d4aa6b88e1ac790f2b6bfc0a6
tony9402 <tony9402@naver.com>
2021-04-25 04:00:04 +0900

    [ADD] 1368 solution

---
+++ solution/minimum_spanning_tree/1368/main.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int uf[303];
+
+int find(int x) {
+    if(uf[x] < 0) return x;
+    return uf[x] = find(uf[x]);
+}
+
+bool merge(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if(a == b)return false;
+    uf[b] = a;
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N; cin >> N;
+    vector<tuple<ll, int, int>> w;
+    for(int i=0;i<=N;i++) uf[i] = -1;
+    for(int i=0;i<N;i++) {
+        int x;cin >> x;
+        w.emplace_back(x, i, N);
+    }
+    for(int i=0;i<N;i++) {
+        for(int j=0;j<N;j++) {
+            int x; cin >> x;
+            if(i >= j) continue;
+            w.emplace_back(x, i, j);
+        }
+    }
+
+    sort(w.begin(), w.end());
+    ll answer = 0;
+    for(auto &[W, a, b]: w) {
+        if(merge(a, b)) answer += W;
+    }
+    cout << answer;
+
+    return 0;
+}
