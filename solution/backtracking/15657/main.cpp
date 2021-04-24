40273185ee0994e77c331165b906ee1adae88835
tony9402 <tony9402@naver.com>
2021-04-25 04:54:48 +0900

    [ADD] 15657 solution

---
+++ solution/backtracking/15657/main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int arr[10], choose[10], N, M;
+
+void dfs(int idx, int cnt) {
+    if(cnt == M) {
+        for(int i=0;i<M;i++) {
+            cout << arr[choose[i]] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=idx;i<N;i++) {
+        choose[cnt] = i;
+        dfs(i, cnt + 1);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N >> M;
+    for(int i=0;i<N;i++) cin >> arr[i];
+    sort(arr, arr + N);
+    dfs(0, 0);
+
+    return 0;
+}
