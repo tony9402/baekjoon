d132886770eed21937bbf5ff092f11bb14bc726e
tony9402 <tony9402@naver.com>
2021-04-25 05:20:26 +0900

    [ADD] 9663 solutions

---
+++ solution/backtracking/9663/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, ans;
+bool Y[22], DL[44], DR[44];
+
+void dfs(int y, int cnt){
+    if(cnt == n){
+        ans++; 
+        return;
+    }
+    for(int j=0;j<n;j++){
+        if(Y[j] || DL[y + j] || DR[y - j + n])continue;
+        Y[j] = DL[y + j] = DR[y - j + n] = true;
+        dfs(y + 1, cnt + 1);
+        Y[j] = DL[y + j] = DR[y - j + n] = false;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;    
+    dfs(0, 0);
+    cout << ans;
+
+    return 0;
+}
