e71f9aaa5f0b3b3a71ba6a0ef0e567bc5749a771
tony9402 <tony9402@naver.com>
2021-04-28 04:09:06 +0900

    [ADD] 1010 solution

---
+++ solution/dynamic_programming_1/1010/main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+ll DP[33][33];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int T; cin >> T;
+    DP[0][0] = 1;
+    for(int i=1;i<=30;i++) {
+        DP[i][0] = 1;
+        for(int j=1;j<=30;j++) {
+            DP[i][j] = DP[i - 1][j] + DP[i - 1][j - 1];
+        }
+    }
+    while(T--) {
+        int N, M; cin >> N >> M;
+        cout << DP[M][N] << '\n';
+    }
+
+    return 0;
+}
