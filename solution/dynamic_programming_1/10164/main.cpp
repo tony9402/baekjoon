418edcb7e62f08a135cec47ce9e04bfa22e344eb
tony9402 <tony9402@naver.com>
2021-05-04 23:05:43 +0900

    [ADD] 10164 solution

---
+++ solution/dynamic_programming_1/10164/main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int N, M, K;
+int DP[22][22];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N >> M >> K;
+    int y = N, x = M;
+    if(K > 0) {
+        --K;
+        y = K / M + 1, x = K % M + 1; // 1 - based
+    }
+    DP[1][1] = 1;
+    for(int i=1;i<=y; i++) {
+        for(int j=1;j<=x;j++) {
+            if(i == 1 && j == 1) continue;
+            DP[i][j] = DP[i][j - 1] + DP[i - 1][j];
+        }
+    }
+    for(int i=y;i<=N;i++){
+        for(int j=x;j<=M;j++){
+            if(i == y && j == x) continue;
+            DP[i][j] = DP[i][j - 1] + DP[i - 1][j];
+        }
+    }
+    cout << DP[N][M];
+
+    return 0;
+}
