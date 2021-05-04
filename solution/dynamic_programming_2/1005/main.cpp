07911bf7daf13da6c7aacc3237186c4d81d3acfa
tony9402 <tony9402@naver.com>
2021-05-04 22:55:31 +0900

    [ADD] 1005 1516 solutions

---
+++ solution/dynamic_programming_2/1005/main.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T; cin >> T;
+    while(T--) {
+        int N, M, W; cin >> N >> M;
+        vector<vector<int>> graph(N);
+        vector<int> V(N), DP(N), indegree(N);
+        for(int i=0;i<N;i++) cin >> V[i];
+        for(int i=0;i<M;i++) {
+            int a, b; cin >> a >> b;
+            graph[--a].emplace_back(--b);
+            indegree[b]++;
+        }
+        cin >> W;
+
+        queue<int> Q;
+        for(int i=0;i<N;i++){
+            if(!indegree[i]) {
+                Q.push(i);
+                DP[i] = V[i];
+            }
+        } 
+
+        while(!Q.empty()) {
+            int cur = Q.front(); Q.pop();
+            for(auto &nxt: graph[cur]) {
+                if(--indegree[nxt] == 0) Q.push(nxt);
+                DP[nxt] = max(DP[nxt], DP[cur] + V[nxt]);
+            }
+        }
+
+        cout << DP[--W] << '\n';
+    }
+
+    return 0;
+}
---
+++ solution/dynamic_programming_2/1516/main.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N; cin >> N;
+    vector<vector<int>> graph(N);
+    vector<int> V(N), DP(N), indegree(N);
+    for(int i=0;i<N;i++) {
+        cin >> V[i];
+        while(true) {
+            int x; cin >> x;
+            if(x == -1) break;
+            graph[--x].push_back(i);
+            indegree[i] ++;
+        }
+    }
+
+    queue<int> Q;
+    for(int i=0;i<N;i++) {
+        if(!indegree[i]) {
+            Q.push(i);
+            DP[i] = V[i];
+        }
+    }
+
+    while(!Q.empty()) {
+        int cur = Q.front(); Q.pop();
+        for(auto &nxt: graph[cur]) {
+            if(--indegree[nxt] == 0) Q.push(nxt);
+            DP[nxt] = max(DP[nxt], DP[cur] + V[nxt]);
+        }
+    }
+    for(int i=0;i<N;i++) cout << DP[i] << '\n';
+
+    return 0;
+}
