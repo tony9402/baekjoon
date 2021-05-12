ed0851657c9b4721b156ffb39fe01f4d629c2c51
OhSeungYoung <osy0056@khu.ac.kr>
2021-05-12 18:22:39 +0900

    [UDT] input 함수 정의 추가, 오타 수정

--- solution/dynamic_programming_1/17626/main.py
+++ solution/dynamic_programming_1/17626/main.py
@@ -1,10 +1,15 @@
 # Authored by : osy0056
 # Co-authored by : -
-# Link : http://boj.kr/782a2e02166c4b5cba7c42b19d55a7e5
+# Link : http://boj.kr/49cc21bd78694086b7b2864e8bc6fa69
 import math
+import sys
 
 
-[n] = [*map(lambda x: int(x), input().split())]
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+n = int(input())
 dp = [0]*50001
 INF = float('inf')
 
@@ -12,7 +17,7 @@ dp[0] = 0
 dp[1] = 1
 for i in range(2, n+1):
     min_value = INF
-    for k in range(1, math.floor(math.sqrt(k))+1):
+    for k in range(1, math.floor(math.sqrt(i))+1):
         min_value = min(min_value, dp[i-k*k])
     dp[i] = min_value+1
 print(dp[n])
