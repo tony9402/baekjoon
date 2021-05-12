68ec9ec355c1b9a34989c29d9de727a48fbabe5e
MS Kim(tony9402) <tony9402@naver.com>
2021-05-12 18:41:55 +0900

    [UPD] 공백 사소한 정리

--- solution/dynamic_programming_1/17626/main.py
+++ solution/dynamic_programming_1/17626/main.py
@@ -4,20 +4,18 @@
 import math
 import sys
 
-
 def input():
     return sys.stdin.readline().rstrip()
 
-
 n = int(input())
-dp = [0]*50001
+dp = [0] * 50001
 INF = float('inf')
 
 dp[0] = 0
 dp[1] = 1
 for i in range(2, n+1):
     min_value = INF
-    for k in range(1, math.floor(math.sqrt(i))+1):
-        min_value = min(min_value, dp[i-k*k])
-    dp[i] = min_value+1
+    for k in range(1, math.floor(math.sqrt(i)) + 1):
+        min_value = min(min_value, dp[i - k * k])
+    dp[i] = min_value + 1
 print(dp[n])
