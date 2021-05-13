ee7f2521aed0a70df4fdf82bea13c11f1c3b7721
hojin <redsmile123@naver.com>
2021-05-13 15:35:53 +0900

    변수명 내장함수와 충돌 수정

--- solution/graph_traversal/16174/main.py
+++ solution/graph_traversal/16174/main.py
@@ -8,18 +8,18 @@ def input():
     return sys.stdin.readline().rstrip()
 
 N = int(input())
-map = [list(map(int, input().split())) for _ in range(N)]
+maps = [list(map(int, input().split())) for _ in range(N)]
 inf = float('inf')
 q = deque()
 q.append((0, 0))
 while q:
     x, y = q.popleft()
     for dx, dy in ((0, 1), (1, 0)):
-        nx, ny = x + dx * map[x][y], y + dy * map[x][y]
+        nx, ny = x + dx * maps[x][y], y + dy * maps[x][y]
         if 0 <= nx < N and 0 <= ny < N:
             if nx == N - 1 and ny == N - 1:
                 print('HaruHaru')
                 sys.exit(0)
             q.append((nx, ny))
-    map[x][y] = inf
+    maps[x][y] = inf
 print('Hing')
