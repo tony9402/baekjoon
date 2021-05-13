b6439ba988dafad1799b3c3274b8b2359bfdc09a
MS Kim(tony9402) <tony9402@naver.com>
2021-05-13 15:50:50 +0900

    [UPD] Refactoring

--- solution/graph_traversal/14716/main.py
+++ solution/graph_traversal/14716/main.py
@@ -1,6 +1,6 @@
-# // Authored by : chj3748
-# // Co-authored by : -
-# // Link : http://boj.kr/b5d154fcfb1c46e6a99e702c35f4eb26
+# Authored by : chj3748
+# Co-authored by : tony9402
+# Link : http://boj.kr/b5d154fcfb1c46e6a99e702c35f4eb26
 import sys
 
 def input():
@@ -8,18 +8,19 @@ def input():
 
 M, N = map(int, input().split())
 banner = [list(map(int, input().split())) for _ in range(M)]
+dirx = (1, 0, -1, 0, 1, 1, -1, -1)
+diry = (0, 1, 0, -1, 1, -1, 1, -1)
 
 def find_string(row, col):
     stack = [(row, col)]
     banner[row][col] = 0
     while stack:
         x, y = stack.pop()
-        for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)):
+        for dx, dy in zip(dirx, diry):
             nx, ny = x + dx, y + dy
-            if 0 <= nx < M and 0 <= ny < N:
-                if banner[nx][ny]:
-                    stack.append((nx, ny))
-                    banner[nx][ny] = 0
+            if 0 <= nx < M and 0 <= ny < N and banner[nx][ny]:
+                stack.append((nx, ny))
+                banner[nx][ny] = 0
     return 1
 
 string_cnt = 0
