f8c0d21762bb4b4a9393eddd009438706d2cd0a4
MS Kim(tony9402) <tony9402@naver.com>
2021-05-13 15:57:35 +0900

    [UPD] Refactoring

--- solution/graph_traversal/16174/main.py
+++ solution/graph_traversal/16174/main.py
@@ -1,5 +1,5 @@
 # // Authored by : chj3748
-# // Co-authored by : -
+# // Co-authored by : tony9402
 # // Link : http://boj.kr/4d17bddef7af409ebdb384693b3f96d9
 import sys
 from collections import deque
@@ -7,19 +7,24 @@ from collections import deque
 def input():
     return sys.stdin.readline().rstrip()
 
+dirx = (0, 1)
+diry = (1, 0)
 N = int(input())
 maps = [list(map(int, input().split())) for _ in range(N)]
 inf = float('inf')
+
 q = deque()
 q.append((0, 0))
 while q:
     x, y = q.popleft()
-    for dx, dy in ((0, 1), (1, 0)):
+    if x == N - 1 and y == N - 1:
+        print('HaruHaru')
+        exit(0)
+
+    for dx, dy in zip(dirx, diry):
         nx, ny = x + dx * maps[x][y], y + dy * maps[x][y]
         if 0 <= nx < N and 0 <= ny < N:
-            if nx == N - 1 and ny == N - 1:
-                print('HaruHaru')
-                sys.exit(0)
             q.append((nx, ny))
     maps[x][y] = inf
+
 print('Hing')
