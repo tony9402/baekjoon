38a32ca5816a5919d7d209794799cdf7af9107db
MS Kim(tony9402) <tony9402@naver.com>
2021-06-05 02:42:13 +0900

    [UDP] 간단한 업데이트

--- solution/dynamic_programming_1/1010/main.py
+++ solution/dynamic_programming_1/1010/main.py
@@ -4,13 +4,17 @@
 import sys
 def input():
     return sys.stdin.readline().rstrip()
+
 T = int(input())
+
 dp = [[0 for _ in range(31)] for _ in range(31)]
 dp[0][0] = 1
+
 for num in range(1,31):
     dp[num][0] = 1
     for pick in range(1,31):        
         dp[num][pick] = dp[num-1][pick] + dp[num-1][pick-1]
+
 for _ in range(T):
-    N,M = map(int,input().split())
+    N, M = map(int,input().split())
     print(dp[M][N])
