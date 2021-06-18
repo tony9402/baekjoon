dfbe7560b650f77694e4a58970ea31288223fb54
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-06-18 09:14:00 +0900

    [ADD] 16953 python solution

--- solution/greedy/16953/main.py
+++ solution/greedy/16953/main.py
@@ -1,30 +1,29 @@
 # Authored by : gusdn3477
 # Co-authored by : -
-# Link : http://boj.kr/102afab5148c43ad8c8d179d80117e24
+# Link : http://boj.kr/e6b9e893d80948c5b739caaf3030e55f
 import sys
-from collections import deque
 
 def input():
     return sys.stdin.readline().rstrip()
 
-def BFS(x):
+def DFS(x,ct):
 
-    queue = deque()
-    queue.append((x,1))
+    if x == B:
+        ans.append(ct)
+        return
 
-    while queue:
-        q = queue.popleft()
-        if q[0] == B:
-            return q[1]
+    if x * 10 + 1 <= B:
+        DFS(x * 10 + 1,ct+1)
 
-        if q[0] * 10 + 1 <= B:
-            queue.append((q[0] * 10 + 1, q[1]+1))
-
-        if q[0] * 2 <= B:
-            queue.append((q[0] * 2, q[1]+1))
-
-    return -1
+    if x * 2 <= B:
+        DFS(x*2,ct+1)
 
 A, B = map(int, input().split())
-ans = BFS(A)
-print(ans)
+ans = []
+DFS(A,1)
+
+if ans:
+    print(ans[0])
+
+else:
+    print(-1)
