7786b9bbc6fe0f10f7215dd4c5ce60817dde37b2
MS Kim(tony9402) <tony9402@naver.com>
2021-06-18 18:36:59 +0900

    [UPD] Refactoring

--- solution/data_structure2/1927/main.py
+++ solution/data_structure2/1927/main.py
@@ -1,7 +1,6 @@
 # Authored by : gusdn3477
 # Co-authored by : -
 # Link : http://boj.kr/5d73f6bad59b42a58688435c855b9580
-
 import sys
 import heapq
 
@@ -12,14 +11,11 @@ N = int(input())
 heap = []
 
 for i in range(N):
-
     x = int(input())
     if x == 0:
         if not heap:
             print(0)
-
         else:
             print(heapq.heappop(heap))
-
     else:
         heapq.heappush(heap,x)
