df732e3377dfbec315b585f49ede4d6b1e998720
MS Kim(tony9402) <tony9402@naver.com>
2021-06-20 11:38:26 +0900

    [UPD] Refactoring

--- solution/dynamic_programming_1/11726/main.py
+++ solution/dynamic_programming_1/11726/main.py
@@ -6,12 +6,13 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
+MOD = 10007
 N = int(input())
 dp = [0] * (N+2)
 dp[1] = 1
 dp[2] = 2
 
 for i in range(3,N+1):
-    dp[i] = dp[i-1] % 10007 + dp[i-2] % 10007
+    dp[i] = (dp[i-1] + dp[i-2]) % MOD
 
-print(dp[N] % 10007)
+print(dp[N] % MOD)
