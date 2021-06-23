c115d0f72b9bfb1360fb8b0c8d0ff341315eb834
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-06-23 15:22:38 +0900

    [ADD] 10798 python solution

---
+++ solution/string/10798/main.py
@@ -0,0 +1,21 @@
+# Authored by : gusdn3477
+# Co-authored by : -
+# Link : http://boj.kr/a1c26275e6af44f5a080d54a61b4c677
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+arr = []
+ans = ''
+max_len = 0
+for i in range(5):
+    a = input()
+    max_len = max(max_len, len(a))
+    arr.append(a)
+for i in range(max_len):
+    for j in range(5):
+        try:
+            ans += arr[j][i]
+        except:
+            continue
+print(ans)
