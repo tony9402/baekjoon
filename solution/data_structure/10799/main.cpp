3705d15c7aa5ef8d6358bd9ba2bc6e9b5f2075a4
tallua <talluay@gmail.com>
2021-07-01 22:55:56 +0900

    [ADD] 10799 cpp solution

---
+++ solution/data_structure/10799/main.cpp
@@ -0,0 +1,40 @@
+// Authored by : tallua
+// Co-authored by : -
+// Link : http://boj.kr/53875935b49b422486b56441978f7ad7
+#include <iostream>
+
+using namespace std;
+using ull = unsigned long long;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ull total = 0;
+    ull pipe_stack = 0;
+    bool is_opened = true;
+
+    char ch;
+    while (cin >> ch) {
+        if (ch == '(') {
+            // start of pipe
+            pipe_stack++;
+            is_opened = true;
+        } else if (ch == ')' && is_opened) {
+            // laser
+            pipe_stack--;
+            total += pipe_stack;
+            is_opened = false;
+        } else if (ch == ')' && !is_opened) {
+            // end of pipe
+            pipe_stack--;
+            total += 1;
+            is_opened = false;
+        }
+    }
+
+    cout << total << '\n';
+
+    return 0;
+}
