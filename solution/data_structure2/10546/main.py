1279fd02e22af4fc66ca8b5426502247627e9205
MS Kim(tony9402) <tony9402@naver.com>
2021-07-05 17:10:39 +0900

    [UPD] 일부 코드 단순화

--- solution/data_structure2/10546/main.py
+++ solution/data_structure2/10546/main.py
@@ -10,14 +10,12 @@ N = int(input())
 dic = {}
 for i in range(N):
     name = input()
-    if name not in dic:
-        dic[name] = 1
-    else:
-        dic[name] += 1
+    dic[name] = dic.get(name, 0) + 1
 
 for i in range(N-1):
     name = input()
     dic[name] -= 1
+
 dic_list = list(dic.items())
 dic_list.sort(key = lambda x : (-x[1], x[0]))
 print(dic_list[0][0])
