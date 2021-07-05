0c29aec2e1d016c64a3b64576d8c454136ba4bee
MS Kim(tony9402) <tony9402@naver.com>
2021-07-05 17:18:08 +0900

    [UPD] Refactoring

--- solution/math/11653/main.py
+++ solution/math/11653/main.py
@@ -7,11 +7,10 @@ def input():
     return sys.stdin.readline().rstrip()
 
 N = int(input())
-while True:
-    if N == 1:
+X = N
+for i in range(2, N + 1):
+    if X == 1:
         break
-    for i in range(2,N+1):
-        if N % i == 0:
-            print(i)
-            N = N // i
-            break
+    while X % i == 0:
+        X //= i
+        print(i)
