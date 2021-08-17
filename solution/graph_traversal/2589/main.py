6072d24e4fdd7b03f0450d71f07f589e1cb8eda7
MS Kim(tony9402) <tony9402@naver.com>
2021-08-18 00:22:25 +0900

    [UPD] 1 Tab -> 4 spaces

--- solution/graph_traversal/2589/main.py
+++ solution/graph_traversal/2589/main.py
@@ -9,32 +9,34 @@ def input():
     return sys.stdin.readline().rstrip()
 
 def BFS(x,y):
-	queue = deque()
-	queue.append((x,y,0))
-	visited[x][y] = 1
-	while queue:
-		q = queue.popleft()
-		for i in range(4):
-			dx = nx[i] + q[0]
-			dy = ny[i] + q[1]
-			if dx < 0 or dx >= L or dy < 0 or dy >= W:
-				continue
-			if visited[dx][dy] == 0 and arr[dx][dy] == 'L':
-				visited[dx][dy] = 1
-				queue.append((dx,dy,q[2]+1))
-	return q[2]
+    queue = deque()
+    queue.append((x,y,0))
+    visited[x][y] = 1
+    while queue:
+        q = queue.popleft()
+        for i in range(4):
+            dx = nx[i] + q[0]
+            dy = ny[i] + q[1]
+            if dx < 0 or dx >= L or dy < 0 or dy >= W:
+                continue
+            if visited[dx][dy] == 0 and arr[dx][dy] == 'L':
+                visited[dx][dy] = 1
+                queue.append((dx,dy,q[2]+1))
+    return q[2]
 
 L, W = map(int, input().split())
 arr = []
 nx = [-1, 0, 1, 0]
 ny = [0, -1, 0, 1]
 MIN_CT = 0
+
 for i in range(L):
-	arr.append(input())
+    arr.append(input())
+
 for i in range(L):
-	for j in range(W):
-		if arr[i][j] == 'L':
-			visited = [[0 for i in range(W)] for j in range(L)]
-			ct = BFS(i,j)
-			MIN_CT = max(MIN_CT, ct)
+    for j in range(W):
+        if arr[i][j] == 'L':
+            visited = [[0 for i in range(W)] for j in range(L)]
+            ct = BFS(i,j)
+            MIN_CT = max(MIN_CT, ct)
 print(MIN_CT)
