d68e540f9985462b072fbd23784764cd0469f480
witch-factory <soakdma37@gmail.com>
2021-08-30 23:39:22 +0900

    [ADD] 20365 cpp solution

---
+++ solution/greedy/20365/main.cpp
@@ -0,0 +1,33 @@
+﻿// Authored by : dart
+// Co-authored by : -
+// Link : http://boj.kr/d9103d0acfdd4bd5b7ff71735c719471
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int n;
+string word;
+int red = 0, blue = 0;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> word;
+    
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            if (word[i] == 'B') { blue++; }
+            if (word[i] == 'R') { red++; }
+        }
+        else {
+            if (word[i] == 'B' && word[i - 1] == 'R') { blue++; }
+            if (word[i] == 'R' && word[i - 1] == 'B') { red++; }
+        }
+    }
+    cout << min(red, blue) + 1 << "\n";
+
+
+    return 0;
+}
