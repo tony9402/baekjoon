04d039f36450babc4fbdfa77e32acca4ce988229
MS Kim(tony9402) <tony9402@naver.com>
2021-08-30 16:14:26 +0900

    [UPD] Refactoring

--- solution/disjoint_set/17352/main.cpp
+++ solution/disjoint_set/17352/main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 #define SET_SIZE 300010
 
-typedef struct DisjointSet{
+struct DisjointSet{
     int parent[SET_SIZE];
     
     int find(int x){
@@ -20,22 +20,26 @@ typedef struct DisjointSet{
     DisjointSet(){
         for(int i=0; i<SET_SIZE; i++) parent[i]=i;
     }
-}DisjointSet;
-DisjointSet st;
-
-int N;
+} st;
 
 int main()
 {
     ios::sync_with_stdio(false); cin.tie(0);
-    int a,b, prev;
     
-    cin>>N;
-    for(int i=0; i<N-2; i++){cin>>a>>b;  st.Union(a,b);}
+    int N, a, b, prev;
+    cin >> N;
+    for(int i=0; i < N-2; i++) {
+        cin>>a>>b;
+        st.Union(a,b);
+    }
     
     prev = st.find(1);
+    
     for(int i=1; i<=N; i++){
-        if(st.find(i) != prev){cout<<prev<<" "<<st.find(i)<<"\n";  break;}
+        if(st.find(i) != prev){
+            cout << prev << " " << st.find(i) << "\n";
+            break;
+        }
         prev = st.find(i);
     }
     
