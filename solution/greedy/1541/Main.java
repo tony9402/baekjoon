dff581315cc4b1d3d3ab9f59760cc32b85e9ea31
suinj8 <90179555+suinj8@users.noreply.github.com>
2021-11-28 22:10:46 +0900

    [ADD] 1541 java solution
    주어지는 식에 괄호를 쳐서 최소값을 구하는 문제입니다.
    입력을 기호를 포함하여 토큰화 한 뒤
    첫번째 마이너스를 만나기 전까지 모든 수를 더하고 그 이후의 수는 모두 빼면 최소값이 됩니다.

---
+++ solution/greedy/1541/Main.java
@@ -0,0 +1,67 @@
+package baekjoon_1541;
+
+//Authored by : suin8
+//Co-authored by : -
+//Link : 
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FastReader rd = new FastReader();
+		
+		String input = rd.nextLine();
+		//입력받은 문장을 +,-를 포함하여 토큰화
+		StringTokenizer stk = new StringTokenizer(input, "+ | -", true);
+		//첫 마이너스 이전은 모두 더하고 
+		//이후에 모든 수를 빼면 최소값이 된다.
+		boolean minus = false;
+		int sum = 0;
+		while(stk.hasMoreTokens()) {
+			String token = stk.nextToken();
+			
+			if(token.equals("+")) continue;
+			else if(token.equals("-")) minus = true;
+			else {
+				if(minus == true) sum -= Integer.parseInt(token);
+				else sum += Integer.parseInt(token);
+			}
+		}
+		
+		System.out.print(sum);
+	}
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
