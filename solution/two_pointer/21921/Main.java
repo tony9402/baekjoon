e5318d5bbaa07801da5ea48a0e1d57ffcaee1705
suinj8 <90179555+suinj8@users.noreply.github.com>
2021-11-28 23:21:18 +0900

    [ADD] 21921 java solution
    최대 누적방문자 수와 최대 방문자가 왔던 날이 총 며칠인지 구하는 문제입니다.
    우선 입력을 받으면서 i날까지의 누적 방문자 수를 구해놓고
    슬라이딩 윈도우 방식으로 X일만큼씩 확인해보고 값을 구합니다.

---
+++ solution/two_pointer/21921/Main.java
@@ -0,0 +1,75 @@
+package baekjoon_21921;
+
+//Authored by : suin8
+//Co-authored by : -
+//Link : http://boj.kr/1c1a979d8e70465fbe6b2aed1835549a
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int[] day = new int[250010];
+	static int[] sum = new int[250010];
+	
+	public static void main(String[] args) {
+		FastReader rd = new FastReader();
+
+		int N = rd.nextInt();
+		int X = rd.nextInt();
+		//i번째까지 누적 방문자 수 구하기
+		for(int i = 1;i <= N;i++) {
+			day[i] = rd.nextInt();
+			sum[i] = sum[i - 1] + day[i];
+		}
+		
+		int max = 0; //최대로 방문한 누적 방문자 수
+		int cnt = 0; //최대로 방문한 날의 수
+		for(int i = X;i <= N;i++) {
+			//슬라이딩 윈도우 방식으로 X일만큼 방문자 수를 구해서
+			int tmp = sum[i] - sum[i - X];
+			//max값보다 크면 max값 갱신 후
+			//기간을 1로 초기화
+			if(tmp > max) {
+				max = tmp;
+				cnt = 1;
+			}
+			//max값과 동일한 날이 또 있는경우 기간증가
+			else if(tmp == max) cnt++;
+		}
+		
+		if(max == 0) System.out.println("SAD");
+		else System.out.println(max + "\n" + cnt);
+	}
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
