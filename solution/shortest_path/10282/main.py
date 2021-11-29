68a63d218ca32bd47af1e823ab92ec6652635a72
MS Kim(tony9402) <tony9402@naver.com>
2021-11-30 05:17:57 +0900

    [UPD] Refactoring

--- solution/shortest_path/10282/main.py
+++ solution/shortest_path/10282/main.py
@@ -1,6 +1,6 @@
 # Authored by : yj2221
-# Co-authored by : -
-# Link : http://boj.kr/22ca7154f6ec4ff0b8749054c40fa809
+# Co-authored by : tony9402
+# Link : http://boj.kr/b4c107ca25f442e8bef9f8486e248741
 
 import sys
 import heapq
@@ -8,34 +8,28 @@ import heapq
 def input():
     return sys.stdin.readline().rstrip()
 
-T = int(input())
+INF = 99999999
 
-def dijk(start, dependency):
+def dijkstra(start, dependency):
     pq = [[0,start]]
-    result = [99999999 for _ in range(n+1)]
+    result = [INF for _ in range(n+1)]
     result[start] = 0
     while pq:
         cur_time, cur = heapq.heappop(pq)
-        for _next, next_time in dependency[cur].items():
+        for next_com, next_time in dependency[cur].items():
             time = cur_time + next_time
-            if result[_next]>time:
-                result[_next] = time
-                heapq.heappush(pq,[time, _next])
+            if result[next_com] > time:
+                result[next_com] = time
+                heapq.heappush(pq, [time, next_com])
     return result
 
-for _ in range(T):
+for testcase in range(int(input())):
     n, d, c = map(int,input().split())
     dependency = {i:{} for i in range(1,n+1)}
     for _ in range(d):
         a, b, s = map(int,input().split())
         dependency[b][a] = s
         
-    result = dijk(c,dependency)
-    cnt = 0
-    _max = 0
-    for value in result:
-        if value==99999999:
-            continue
-        cnt+=1
-        _max = max(_max, value)
-    print(cnt, _max)
+    result = dijkstra(c, dependency)
+    result = [value for value in result if value != INF]
+    print(len(result), max(result))
