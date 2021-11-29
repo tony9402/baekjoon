3cb73262d08f5cbda54f08a61d2fa70bc9a022cf
MS Kim(tony9402) <tony9402@naver.com>
2021-11-30 05:46:28 +0900

    [UPD] Refactoring

--- solution/shortest_path/main.py
+++ solution/shortest_path/main.py
@@ -7,34 +7,35 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
+INF = 10000000
 def dijkstra(road, start):
-    dp = [10000000]*(n+1)
+    dp = [INF]*(n+1)
     dp[start] = 0
     heap = []
     heappush(heap, [0, start])
 
     while heap:
-        time, cur = heappop(heap)
+        cur_time, cur = heappop(heap)
 
-        if time > dp[cur]: #기존 경로 사용
+        if cur_time > dp[cur]: #기존 경로 사용
             continue
 
-        for t, nei in road[cur]:
-            new_t = t + time
-            if new_t < dp[nei]:
-                dp[nei] = new_t
-                heappush(heap, [new_t, nei])
+        for t, nxt in road[cur]:
+            new_time = t + cur_time
+            if new_time < dp[nxt]:
+                dp[nxt] = new_time
+                heappush(heap, [new_time, nxt])
     return dp
 
 n, m, k = map(int, input().split())
 road = [[] for _ in range(n+1)]
-road_r = [[] for _ in range(n+1)]
+road_rev = [[] for _ in range(n+1)]
 for _ in range(m):
     x, y, t = map(int, input().split())
     road[x].append([t, y])
-    road_r[y].append([t, x])
+    road_rev[y].append([t, x])
     
 dp = dijkstra(road, k)
-dp_r = dijkstra(road_r, k)
+dp_rev = dijkstra(road_rev, k)
 
-print(max([dp[i]+dp_r[i] for i in range(1, n+1)]))
+print(max([dp[i]+dp_rev[i] for i in range(1, n+1)]))
