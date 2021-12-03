be663de6ceee43790180cdf02d90944fa199c11b
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-03 16:35:57 +0900

    [ADD] 20922 cpp solution
    투포인터 문제풀이 입니다.
    K개를 포함하지 않을때는 end포인터를 늘려 최대 길이를 늘려주고
    K개를 초과하면 begin포인터를 늘려 K개이하로 개수를 떨어뜨리면서 최대값을 구합니다.

---
+++ solution/two_pointer/20922/main.cpp
@@ -0,0 +1,41 @@
+//Authored by : suin8
+//Co-authored by : -
+//Link : http://boj.kr/bf868d6487744424a792c188673bf339
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int num[200010];
+int cnt[200010];
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    for(int i = 1;i <= N;i++)
+        cin >> num[i];
+    
+    //start와 end사이에 겹치는 수가 K개 이하일 때는 end를 늘리며
+    //최대 길이를 늘리고
+    //K개 초과일 때는 begin를 늘리며 겹치는 수가 빠질때까지 begin을 증가
+    int begin = 1, end = 1, _max = 0;
+    while(end <= N){
+        if(cnt[num[end]] < K){
+            _max = max(end - begin + 1, _max);
+            cnt[num[end]]++;
+            end++;
+        }
+        else{
+            _max = max(end - begin, _max);
+            cnt[num[begin]]--;
+            begin++;
+        }
+    }
+
+    cout << _max;
+
+    return 0;
+} 
