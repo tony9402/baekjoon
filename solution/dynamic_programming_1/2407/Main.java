6ea880aa07b55587207cf7b7b4461f680ce6377f
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 00:28:25 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가
    코드 설명 변경

--- solution/dynamic_programming_1/2407/Main.java
+++ solution/dynamic_programming_1/2407/Main.java
@@ -8,30 +8,30 @@ import java.math.*;
 
 public class Main {
     static BigInteger[][] dp = new BigInteger[110][110]; 
-	
+
     public static void main(String[] args) {
         FastReader rd = new FastReader();
 
         int n = rd.nextInt();
         int m = rd.nextInt();
-		
-        //매우매우 큰수가 나오는 조합(n C m), 따라서 biginteger사용
+
+        // 매우매우 큰수가 나오는 조합(n C m), 따라서 biginteger사용
         dp[0][0] = new BigInteger("1");
-		
+
         for(int i = 1;i <= n;i++) {
             dp[i][0] = new BigInteger("1");
             for(int j = 1;j <= i;j++) {
-                //biginteger배열이라 null로 초기화 되어 있기 때문에
-                //각 예외상황마다 값을 정해주어야 한다. 
+                // biginteger는 string이라서 null로 초기화 되어 있기 때문에
+                // 각 예외상황마다 값을 정해주어야 한다. 
                 if(i == j) dp[i][j] = new BigInteger("1");
                 else if(i < j) dp[i][j] = new BigInteger("0");		 
                 else dp[i][j] = dp[i - 1][j - 1].add(dp[i - 1][j]);	
             }
         }
-		
+
         System.out.println(dp[n][m]);
     }
-	
+
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
