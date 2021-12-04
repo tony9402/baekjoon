f05da018775df6bbbf167aa353dc65b0e21f8f11
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 01:21:02 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가

--- solution/math/22864/Main.java
+++ solution/math/22864/Main.java
@@ -1,6 +1,7 @@
 //Authored by : suin8
 //Co-authored by : -
 //Link : http://boj.kr/9e22093a088c4facb197d261767247ba
+
 import java.util.*;
 import java.io.*;
 
@@ -14,17 +15,17 @@ public class Main {
         int M = rd.nextInt();
 
         int time = 0;
-        int tired = 0; //피로도
+        int tired = 0; // 피로도
         int work = 0;
 
-        while(time < 24) { //24시간 동안 일
+        while(time < 24) { // 24시간 동안 일
             if(can_work(A, tired, M)) { // 일을 할 수 있을 때
                 work += B;
                 tired += A;
             }
             else { // 일을 할 수 없을 때 => 쉰다.
                 tired -= C;
-                if(tired < 0) tired = 0; //피로도가 0보다 작아지지 않게
+                if(tired < 0) tired = 0; // 피로도가 0보다 작아지지 않게
             }
             time++;
         }
