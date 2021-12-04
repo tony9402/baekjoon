3ff9f38473e7b52ab492e6921295eb02032bcaf7
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 00:22:05 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가

--- solution/dynamic_programming_1/1912/Main.java
+++ solution/dynamic_programming_1/1912/Main.java
@@ -6,28 +6,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
+
     static int[] arr = new int[100010];
     static int[] dp = new int[100010];
- 	
+
     public static void main(String[] args) {
         FastReader rd = new FastReader();
-		
+
         int n = rd.nextInt();
         for(int i = 0;i < n;i++) 
             arr[i] = rd.nextInt();
-		
-        //초기화
+
+        // 초기화
         int max = arr[0];
         dp[0] = arr[0];
-		
-        //dp[i]안에는 arr[0~i]까지의 합 중 최대값을 가진다.
-        //max는 dp값중 최대값을 가진다.=> 결과값
+
+        // dp[i]안에는 arr[0~i]까지의 합 중 최대값을 가진다.
+        // max는 dp값중 최대값을 가진다.=> 결과값
         for(int i = 1;i < n;i++) {
             dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
             max = Math.max(dp[i], max);
         }
-		
+
         System.out.println(max);
     }
 
