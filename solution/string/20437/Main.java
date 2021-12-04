bdb392ad328b3b27c9e78f7cf074ab10c738f348
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 01:14:06 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가
    코드설명 주석 수정

--- solution/string/20437/Main.java
+++ solution/string/20437/Main.java
@@ -4,9 +4,9 @@
 
 /* 
 3. 어떤문자를 정확히 k개 포함하는 가장 짧은 문자열
-    == 시작과 끝이 어떤 문자인 가장 짧은 문자열
-4. 정확히 k개 포함하는 가장 긴 문자열
-    == 시작과 끝이 어떤 문자인 가장 긴 문자열  
+== 시작과 끝이 어떤 문자인 가장 짧은 문자열
+4. 어떤문자가 시작과 끝이고 정확히 k개 포함하는 가장 긴 문자열
+== 시작과 끝이 어떤 문자인 가장 긴 문자열  
 */
 
 import java.util.*;
@@ -14,12 +14,12 @@ import java.io.*;
 
 public class Main {
     static int[] alpha = new int[26];
-	
+
     public static void main(String[] args) {
         FastReader rd = new FastReader();
-		
+
         int T = rd.nextInt();
-		
+
         for(int i = 0;i < T;i++) {
 
             // 초기화 
@@ -28,24 +28,24 @@ public class Main {
 
             String W = rd.nextLine();
             int K = rd.nextInt();
-			
-            //각 알파벳이 몇번 나왔는지 센다
+
+            // 각 알파벳이 몇번 나왔는지 센다
             for(int j = 0;j < W.length();j++) 
                 alpha[W.charAt(j) - 'a']++;
-			
+
             int min = Integer.MAX_VALUE, max = 0;
             for(int j = 0;j < W.length();j++) {
                 if(alpha[W.charAt(j) - 'a'] < K) continue;
-				
-                //K보다 크거나 같은 빈도로 나온 문자만 본다
-                //그 문자가 문자열의 시작과 끝
+
+                // K보다 크거나 같은 빈도로 나온 문자만 본다
+                // 그 문자가 문자열의 시작과 끝
                 char ch = W.charAt(j);
                 int count = 0;
                 for(int l = j;l < W.length();l++) {
                     if(ch != W.charAt(l)) continue;
                     else count++;
-						
-                    //정확히 K개가 되는 순간 min, max값 갱신
+
+                    // 정확히 K개가 되는 순간 min, max값 갱신
                     if(count == K) {
                         min = Math.min(min, l - j + 1);
                         max = Math.max(max, l - j + 1);
@@ -53,11 +53,12 @@ public class Main {
                     }
                 }
             }
-	
+
             if(min == 10001 || max == 0) System.out.println("-1");
             else System.out.println(min + " " + max);
         }
     }
+
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
