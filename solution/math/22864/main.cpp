f88dd9eed46a8ae4a7fe432844be805cef1d3022
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 01:19:36 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가

--- solution/math/22864/main.cpp
+++ solution/math/22864/main.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-//일을 더 할수있는지 확인하는 함수
-//현재 피로도에 A를 더해서 임계점을 넘지않는지 확인
+// 일을 더 할수있는지 확인하는 함수
+// 현재 피로도에 A를 더해서 임계점을 넘지않는지 확인
 bool can_work(int A, int tired, int M){
     if(tired + A <= M) return true;
     else return false;
@@ -19,16 +19,16 @@ int main() {
     int A, B, C, M;  
     cin >> A >> B >> C >> M;
 
-    //24시간동안 일을 진행
-    //can_work가 true를 반환하면 일을 한다.
-    //false 반환시 쉬어서 피로도를 줄인다.
+    // 24시간동안 일을 진행
+    // can_work가 true를 반환하면 일을 한다.
+    // false 반환시 쉬어서 피로도를 줄인다.
     int time = 24, tired = 0, work = 0;
-    while(time--){
-        if(can_work(A, tired, M)){
+    while(time--) {
+        if(can_work(A, tired, M)) {
             work += B;
             tired += A;
         }
-        else{
+        else {
             tired -= C;
             if(tired < 0) tired = 0;
         }
@@ -36,4 +36,4 @@ int main() {
     cout << work;
 
     return 0;
-} 
+}
