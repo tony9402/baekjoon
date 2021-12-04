09b39a0f0eb0c9e24b9ee67c6a36734fedbbf691
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 00:10:59 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가

--- solution/dynamic_programming_1/1010/Main.java
+++ solution/dynamic_programming_1/1010/Main.java
@@ -7,28 +7,28 @@ import java.io.*;
 
 public class Main {
     static int[][] dp = new int[30][30]; 
-	
+
     public static void main(String[] args) {
         FastReader rd = new FastReader();
-		
+
         int T = rd.nextInt();
-		
-        //조합공식을 이용하여 풀이가능 (M C N)
+
+        // 조합공식을 이용하여 풀이가능 (M C N)
         dp[0][0] = 1;
         for(int j = 1;j < 30;j++) {
             dp[j][0] = 1;
             for(int k = 1;k <= j;k++) 
                 dp[j][k] = dp[j - 1][k - 1] + dp[j - 1][k];
         }
-		
+
         for(int i = 0;i < T;i++) {
             int N = rd.nextInt();
             int M = rd.nextInt();
-	
+
             System.out.println(dp[M][N]);
         }
     }
-	
+
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
