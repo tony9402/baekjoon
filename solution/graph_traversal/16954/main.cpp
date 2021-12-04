5d958939196f8694a8af49aef19707d5968dc57a
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 01:05:16 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가

--- solution/graph_traversal/16954/main.cpp
+++ solution/graph_traversal/16954/main.cpp
@@ -12,17 +12,17 @@ int wall_count = 0;
 int dx[] = {1, 0, -1, 0, 1, -1, 1, -1, 0};
 int dy[] = {0, 1, 0, -1, -1, 1, 1, -1, 0};
 
-void moveWall(){
-    for(int i = 8;i > 0;i--){
-        for(int j = 1;j <= 8;j++){
+void moveWall() {
+    for(int i = 8;i > 0;i--) {
+        for(int j = 1;j <= 8;j++) {
             if(chess[i][j] != '#') continue;
 
             int nextwall = i + 1;
-            if(nextwall <= 8){
+            if(nextwall <= 8) {
                 chess[nextwall][j] = '#';
                 chess[i][j] = '.';
             }
-            else{
+            else {
                 chess[i][j] = '.';
                 wall_count--;
             }
@@ -30,34 +30,33 @@ void moveWall(){
     }
 }
 
-bool bfs(){
+bool bfs() {
     queue<pair<int, int>> q;
     q.push(make_pair(8, 1));
 
-
-    //현재위치에서 다음 위치로 갈 수 있는 곳을 큐에 집어넣고
-    //다음 while roop에서 전에 넣었던 위치를 반드시 다 방문한 후
-    //벽을 움직여야한다. 따라서 check이 queue의 크기를 저장하고
-    //check만큼 다 방문한 후에 moveWall()을 실행한다.
-    //그래야 갈 수 있는 모든 경우의 수를 확인할 수 있다.
-    while(!q.empty()){
+    // 현재위치에서 다음 위치로 갈 수 있는 곳을 큐에 집어넣고
+    // 다음 while roop에서 전에 넣었던 위치를 반드시 다 방문한 후
+    // 벽을 움직여야한다. 따라서 check이 queue의 크기를 저장하고
+    // check만큼 다 방문한 후에 moveWall()을 실행한다.
+    // 그래야 갈 수 있는 모든 경우의 수를 확인할 수 있다.
+    while(!q.empty()) {
         int check = q.size();
 
-        for(int i = 0;i < check;i++){
+        for(int i = 0;i < check;i++) {
             pair cur = q.front();
             q.pop();
 
             if(chess[cur.first][cur.second] == '#') continue;
 
             if((cur.first == 1 && cur.second == 8) || wall_count == 0) return true;
-            
-            //8방향 + 제자리 총 9방향을 검사합니다.
-            for(int j = 0;j < 9;j++){
+
+            // 8방향 + 제자리 총 9방향을 검사합니다.
+            for(int j = 0;j < 9;j++) {
                 int nexty = cur.first + dy[j];
                 int nextx = cur.second + dx[j];
 
                 if(nextx <= 0 || nexty <= 0 || nextx > 8 || nexty > 8) continue;
-				if(visited[nexty][nextx] == true || chess[nexty][nextx] == '#') continue;
+                if(visited[nexty][nextx] == true || chess[nexty][nextx] == '#') continue;
 
                 visited[nexty][nextx] = true;
                 q.push(make_pair(nexty, nextx));
@@ -76,11 +75,11 @@ bool bfs(){
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
 
-    //입력과 동시에 벽 개수를 카운트 합니다.
-    for(int i = 1;i <= 8;i++){
+    // 입력과 동시에 벽 개수를 카운트 합니다.
+    for(int i = 1;i <= 8;i++) {
         string str;
         cin >> str;
-        for(int j = 1;j <= 8;j++){
+        for(int j = 1;j <= 8;j++) {
             chess[i][j] = str[j - 1];
             if(chess[i][j] == '#') wall_count++;
         }
