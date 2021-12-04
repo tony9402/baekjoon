ea9b662a73fdcafa736f31e6fa3c0f89fa30b109
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 00:46:22 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가

--- solution/dynamic_programming_1/9655/main.cpp
+++ solution/dynamic_programming_1/9655/main.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
-    
+
     int N;
     cin >> N;
 
-    //짝수일때는 창영이가, 홀수일때는 상근이가 이기게 됩니다.
-    //상근이가 먼저 시작하고 반드시 1개 또는 3개(홀수개)를 가져가기 떄문
+    // 짝수일때는 창영이가, 홀수일때는 상근이가 이기게 됩니다.
+    // 상근이가 먼저 시작하고 반드시 1개 또는 3개(홀수개)를 가져가기 떄문
     cout << ((N % 2 == 0) ? "CY" : "SK");
 
     return 0;
-} 
+}
