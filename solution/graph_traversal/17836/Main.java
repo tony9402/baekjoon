d731b324ceab4c5b6645ffd7e7be725146581bef
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 01:11:14 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가

--- solution/graph_traversal/17836/Main.java
+++ solution/graph_traversal/17836/Main.java
@@ -5,7 +5,7 @@
 import java.util.*;
 import java.io.*;
 
-class Point{
+class Point {
     int x, y;
     Point(int x, int y){
         this.x = x;
@@ -20,18 +20,18 @@ public class Main {
     static int[] dy = {0, 1, 0, -1};
     static boolean[][] visited;
     static Point sword;
-	
+
     public static void main(String[] args) {
         FastReader rd = new FastReader();
-		
+
         N = rd.nextInt();
         M = rd.nextInt();
         T = rd.nextInt();
-		
+
         castle = new int[N + 10][M + 10];
         distance = new int[N + 10][M + 10];
         visited = new boolean[N + 10][M + 10];
-		
+
         for(int i = 1;i <= N;i++) {
             for(int j = 1;j <= M;j++) {
                 castle[i][j] = rd.nextInt();
@@ -40,22 +40,22 @@ public class Main {
             }
         }
 
-        //bfs로 각 지점까지 거리를 구해놓는다.
+        // bfs로 각 지점까지 거리를 구해놓는다.
         bfs();
-		
+
         int use_sword = 0, not_use_sword = 0;
-		
-        //검을 사용했을 때 걸리는 시간. 구하지 못하는 경우는 int형 최대값
+
+        // 검을 사용했을 때 걸리는 시간. 구하지 못하는 경우는 int형 최대값
         if(distance[sword.x][sword.y] != 0)
             use_sword = distance[sword.x][sword.y] + (N - sword.x) + (M - sword.y);
         else use_sword = Integer.MAX_VALUE;
-		
-        //검을 사용하지 않고 걸리는 시간. 구하지 못하는 경우는 int형 최대값
+
+        // 검을 사용하지 않고 걸리는 시간. 구하지 못하는 경우는 int형 최대값
         if(distance[N][M] != 0)
             not_use_sword = distance[N][M];
         else not_use_sword = Integer.MAX_VALUE;
-		
-        //둘 중 작은 값을 T보다 작을때만 출력, 그 외는 Fail출력
+
+        // 둘 중 작은 값을 T보다 작을때만 출력, 그 외는 Fail출력
         if(Math.min(use_sword, not_use_sword) > T)
             System.out.println("Fail");
         else System.out.println(Math.min(use_sword, not_use_sword));
