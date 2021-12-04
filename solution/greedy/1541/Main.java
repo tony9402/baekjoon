2fbd2fabb260dc0e6cc9c0b16ded62b5d077db86
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-05 00:20:14 +0900

    [UPD] Refactoring
    코드 사이사이 tab제거
    주석 뒤 공백 한칸 추가
    코드설명 추가

--- solution/greedy/1541/Main.java
+++ solution/greedy/1541/Main.java
@@ -8,19 +8,21 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         FastReader rd = new FastReader();
-		
+
         String input = rd.nextLine();
-            
-        //입력받은 문장을 +,-를 포함하여 토큰화
+
+        // 입력받은 문장을 +,-를 포함하여 토큰화
         StringTokenizer stk = new StringTokenizer(input, "+ | -", true);
-            
-        //첫 마이너스 이전은 모두 더하고 
-        //이후에 모든 수를 빼면 최소값이 된다.
+
+        // 첫 마이너스 이전은 모두 더하고 
+        // 이후에 모든 수를 빼면 최소값이 된다.
+        // 첫 마이너스 이후에 어떻게 괄호를 치든 전부 뺄 수 있게 만들 수 있다.
+        // - (15 + 20) - (20 + 30) 이런식
         boolean minus = false;
         int sum = 0;
         while(stk.hasMoreTokens()) {
             String token = stk.nextToken();
-			
+
             if(token.equals("+")) continue;
             else if(token.equals("-")) minus = true;
             else {
@@ -28,7 +30,7 @@ public class Main {
                 else sum += Integer.parseInt(token);
             }
         }
-		
+
         System.out.print(sum);
     }
 
