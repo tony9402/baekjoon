2b1c4b1db9f12b9c81bcd484bf7e2f9322f5aabd
MS Kim(tony9402) <tony9402@naver.com>
2021-12-06 02:55:06 +0900

    [UPD] Refactoring

--- solution/dynamic_programming_1/11726/Main.java
+++ solution/dynamic_programming_1/11726/Main.java
@@ -24,10 +24,8 @@ public class Main {
         dp[2] = 2;
 
         // dp[i] = dp[i - 1] + dp[i - 2]
-        if(n >= 3) {
-            for(int i = 3;i <= n;i++) 
-                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-        }
+        for(int i = 3;i <= n;i++) 
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         System.out.println(dp[n]);
     }
 
