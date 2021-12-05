b112838322072be5d69388b0f4434c40e0b5f907
MS Kim(tony9402) <tony9402@naver.com>
2021-12-06 03:23:39 +0900

    [UPD] Refactoring

--- solution/two_pointer/20922/Main.java
+++ solution/two_pointer/20922/Main.java
@@ -1,5 +1,5 @@
 //Authored by : suin8
-//Co-authored by : -
+//Co-authored by : tony9402
 //Link : http://boj.kr/f6232bedc14545ecb59f0398d4826f7d
 
 import java.util.*;
@@ -21,23 +21,15 @@ public class Main {
         // 투포인터로 풀이
         // start와 end사이에 겹치는 수가 K개 이하일 때는 end증가
         // K개 초과일 때는 start를 늘려가며 겹치는 수가 빠질때까지 begin증가
-        int begin = 1, end = 1, max = 0;
-        while(end <= N) {
-            if(count[num[end]] < K) {
-                max = Math.max(end - begin + 1, max);
-                count[num[end]]++;
-                end++;
+        int max = 0;
+        for(int begin = 1, end = 1; begin <= N; begin ++) {
+            while(end <= N && count[num[end]] < K) {
+                count[num[end]] ++;
+                end ++;
             }
-            else {
-                max = Math.max(end - begin, max);
-                // (end - 1) - begin + 1
-                // end번째에 K를 초과하기 때문에 end - 1번째 까지
-                // 길이를 고려해야 함.
-                count[num[begin]]--;
-                begin++;
-            }
+            max = Math.max(end - begin, max);
+            count[num[begin]] --;
         }
-
         System.out.println(max);
     }
 
