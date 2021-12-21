47a789270f6a76700c8c22016aaa9302c0fac42d
suin8 <90179555+suinj8@users.noreply.github.com>
2021-12-21 17:46:29 +0900

    [UPD] 오류 수정
    사용하지 않는 클래스 인자를 삭제하였습니다.

--- solution/data_structure/2493/Main.java
+++ solution/data_structure/2493/Main.java
@@ -25,7 +25,6 @@ public class Main {
     
     public static void main(String[] args) {
         FastReader rd = new FastReader();
-        FastWriter rw = new FastWriter();
         
         int N = rd.nextInt();
         
