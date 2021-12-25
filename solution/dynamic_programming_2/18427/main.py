08818932414aea52183595c42f627bc2943df324
MS Kim(tony9402) <tony9402@naver.com>
2021-12-26 03:01:42 +0900

    [UPD] Refactoring

--- solution/dynamic_programming_2/18427/main.py
+++ solution/dynamic_programming_2/18427/main.py
@@ -1,6 +1,6 @@
 # Authored by : yj2221
-# Co-authored by : -
-# Link : http://boj.kr/9ee437520e914410892bf9cb9b96adad
+# Co-authored by : tony9402
+# Link : http://boj.kr/069cb935ee08462280976f02691b40ca
 
 import sys
 
@@ -8,22 +8,17 @@ def input():
     return sys.stdin.readline().rstrip()
 
 N, M, H = map(int, input().split())
+
 students = []
 for _ in range(N):
     students.append(list(map(int, input().split())))
     
-dp = [0] * (H+1)
-dp[0] = 1
-
+dp = [1] + [0] * (H+1)
 
 for i in range(N):
-    data = []
-    for j in range(H+1):
+    for j in range(H, -1, -1):
         for k in students[i]:
-            if dp[j] != 0 and j + k <= H:
-                data.append((j + k, dp[j]))
-    
-    for height,value in data:
-        dp[height] = (dp[height] + value) % 10007
+            if j - k >= 0:
+                dp[j] = (dp[j] + dp[j - k]) % 10007
 
 print(dp[H])
