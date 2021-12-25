4dd4d40e3712601d7449ea1eb9e7af61f80b0f4c
MS Kim(tony9402) <tony9402@naver.com>
2021-12-26 03:21:03 +0900

    [UPD] Refactoring

--- solution/dynamic_programming_2/14567/main.py
+++ solution/dynamic_programming_2/14567/main.py
@@ -1,6 +1,6 @@
 # Authored by : yj2221
-# Co-authored by : -
-# Link : http://boj.kr/d763c3b0bf67448d841b258e0e12c63e
+# Co-authored by : tony9402
+# Link : http://boj.kr/563199054385492b8f7e25eca46a2696
 
 from collections import deque
 import sys
@@ -12,30 +12,24 @@ n, m = map(int, input().split())
 
 learning = {i:[] for i in range(1, n+1)}
 indegree = [0] * (n+1)
-dp = [9999999] * (n+1)
-visit = [False] * (n+1)
+dp = [1] * (n+1)
 
 for _ in range(m):
     a, b = map(int, input().split())
     learning[a].append(b)
-    indegree[b]+=1
+    indegree[b] += 1
     
 q = deque()
-for i, degree in enumerate(indegree):
-    if i == 0: continue
-    if degree != 0: continue
-    q.append(i)
-    dp[i] = 1
-    visit[i] = True
+for i in range(1,n+1):
+    if indegree[i] == 0:
+        q.append(i)
 
 while q:
     cur = q.popleft()
-    for _next in learning[cur]:
-        if visit[_next]: continue
-        indegree[_next]-=1
-        if indegree[_next] == 0:
-            q.append(_next)
-            visit[_next] = True
-            dp[_next] = min(dp[_next], dp[cur]+1)
-        
+    for nxt in learning[cur]:
+        indegree[nxt] -= 1
+        if indegree[nxt] == 0:
+            q.append(nxt)
+            dp[nxt] = max(dp[nxt], dp[cur]+1)
+
 print(' '.join(map(str,dp[1:])))
