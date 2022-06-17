f8e7a85326370d0fa5ccbf0c682d78be3ca1c0b3
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-17 21:04:18 +0900

    Update main.swift

--- solution/disjoint_set/16562/main.swift
+++ solution/disjoint_set/16562/main.swift
@@ -2,6 +2,25 @@
 // Co-authored by : -
 // Link : http://boj.kr/a5ff34cf2dc845f19b2742d7ab1bc7f9
 
+func find(_ x: Int) -> Int {
+    if x == friend[x] { return x }
+    friend[x] = find(friend[x])
+    return friend[x]
+}
+func merge(_ x: Int, _ y: Int) {
+    let (x, y) = (find(x), find(y))
+    if x == y { return }
+    friend[x] = y
+    if minCost[x] > minCost[y] { minCost[x] = minCost[y] }
+    else { minCost[y] = minCost[x] }
+}
+func isUnion(_ x: Int, _ y: Int) -> Bool {
+    find(x) == find(y)
+}
+func isRoot(_ x: Int) -> Bool {
+    x == friend[x]
+}
+
 var input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, m) = (input[0], input[1]); var k = input[2]
 let cost = [0] + readLine()!.split{$0==" "}.map{Int(String($0))!}
@@ -22,22 +41,3 @@ for i in 1...n {
 }
 
 print( k < 0 ? "Oh no" : sum )
-
-func find(_ x: Int) -> Int {
-    if x == friend[x] { return x }
-    friend[x] = find(friend[x])
-    return friend[x]
-}
-func merge(_ x: Int, _ y: Int) {
-    let (x, y) = (find(x), find(y))
-    if x == y { return }
-    friend[x] = y
-    if minCost[x] > minCost[y] { minCost[x] = minCost[y] }
-    else { minCost[y] = minCost[x] }
-}
-func isUnion(_ x: Int, _ y: Int) -> Bool {
-    find(x) == find(y)
-}
-func isRoot(_ x: Int) -> Bool {
-    x == friend[x]
-}
