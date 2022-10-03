44e4d1dfa5a91f0b80217044e17e6db3528b3f7d
juheon-jeong <76440385+juheon-jeong@users.noreply.github.com>
2022-10-03 16:57:48 +0900

    [ADD] 11441 python solution (update)

--- solution/prefix_sum/11441/main.py
+++ solution/prefix_sum/11441/main.py
@@ -1,7 +1,11 @@
 import sys
-n = int(sys.stdin.readline())
 
-numbers = list(map(int, sys.stdin.readline().split(" ")))
+def input():
+    return sys.stdin.readline()             # 표준 입출력을 위한 함수
+
+n = int(input())
+
+numbers = list(map(int, input().split(" ")))
 solve_list = []                             # 누적합을 저장하기 위한 리스트
 
 sum = 0
@@ -10,10 +14,10 @@ for i in numbers:
     sum += i
     solve_list.append(sum)
 
-testcase = int(sys.stdin.readline())
+testcase = int(input())
 
 for i in range(testcase):
-    parts = list(map(int, sys.stdin.readline().split()))
+    parts = list(map(int, input().split()))
     if parts[0] == 1:                       # 처음부터 j 인덱스까지의 누적합
         print(solve_list[parts[1] - 1])
     else:
