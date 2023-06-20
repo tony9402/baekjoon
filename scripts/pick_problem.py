88642f2a4e2338e930eb986f754442580e522604
tony9402 <tony9402@naver.com>
2023-06-21 08:32:49 +0900

    [UPD] upgrade

--- scripts/pick_problem.py
+++ scripts/pick_problem.py
@@ -1,135 +1,151 @@
-from API import SolvedAPI
-import json
-import subprocess as sp
-import os
 import datetime
-import pytz
+import random
+import json
+import urllib.request as request
 
-def reset():
-    database = dict()
-    with open('./scripts/database.json', 'r') as f:
-        database = json.load(f)
+
+class Date:
+    @staticmethod
+    def get_today_date():
+        utctime = datetime.datetime.utcnow()
+        ksttime = utctime.astimezone(datetime.timezone(datetime.timedelta(hours=9)))
+        return ksttime.year, ksttime.month, ksttime.day
+
+    @classmethod
+    def get_today_random_seed(cls):
+        year, month, day = cls.get_today_date()
+        random.seed(year)
+        first_random_value = random.randint(1, 1073741823)
+        random.seed(month)
+        second_random_value = random.randint(1, 32768)
+        random.seed(day)
+        third_random_value = random.randint(1, 128)
+
+        random_seed = first_random_value + second_random_value + third_random_value
+        return random_seed
+
+
+class Communication:
+    __URL = {
+        "database": "https://raw.githubusercontent.com/tony9402/baekjoon/main/scripts/database.json",
+        "picked_problem": "https://raw.githubusercontent.com/tony9402/baekjoon/main/scripts/picked.json",
+    }
+    __hyperparameter = {
+        "before_days": 20,
+        "n_section": 4,
+        "max_level": [8, 12, 15, 18], # [S3, G4, G1, P3]
+        "choose_count": [1, 2, 1, 1],
+    }
+
+    @staticmethod
+    def get_json(url):
+        text = request.urlopen(url).read()
+        json_data = json.loads(text)
+        return json_data
+
+    @classmethod
+    def get_database(cls):
+        return cls.get_json(cls.__URL['database'])
+
+    @classmethod
+    def get_picked_problem(cls):
+        return cls.get_json(cls.__URL['picked_problem'])
+
+    @classmethod
+    def get_today_problem(cls):
+        today_problems = []
+
+        seed = Date.get_today_random_seed()
+        random.seed(seed)
+
+        all_problem = cls.get_database()
+        picked_problem = cls.get_picked_problem()
+
+        param = cls.__hyperparameter
+        dates = sorted(picked_problem.keys(), reverse=True)[:param['before_days']]
+        used_problem = set()
+        for date in dates:
+            used_problem.update(picked_problem[date])
+
+        used_problem = [[] for section in range(param['n_section'])]
+        splitted_problem = [[] for section in range(param['n_section'])]
+        for problem_id, problem_info in all_problem.items():
+            problemlevel = int(problem_info['problemLevel'])
+            if not 1 <= problemlevel <= 18:
+                continue
+
+            section = 0
+            while section < len(param['max_level']) and problemlevel > param['max_level'][section]:
+                section += 1
+
+            assert section < param['n_section']
+
+            if problem_id in used_problem: 
+                used_problem[section].append(problem_id)
+            else:
+                splitted_problem[section].append(problem_id)
+
+        for section, problems in enumerate(splitted_problem):
+            if len(problems) < param['choose_count'][section]:
+                problems.extend(used_problem[section])
+            problems = random.sample(problems, k=param['choose_count'][section])
+            today_problems.extend(problems)
+
+        return today_problems
+
+
+def get_today_date():
+    year, month, day = Date.get_today_date()
+    timeformat = f"{year:04d}/{month:02d}/{day:02d}"
+    return timeformat
+
+
+def make_table(data: dict, save_file: str) -> None:
+    database = Communication.get_database()
+
+    lines = []
+    for date, problems in data.items():
+        lines.append(f"## {date} \n\n")
+        lines.append("| 번호 | 문제 이름 |\n")
+        lines.append("|:----:|:---------:|\n")
+        for problem_id in problems:
+            problemName = database[problem_id]['problemName']
+            link = f"https://www.acmicpc.net/problem/{problem_id}"
+            line = f"| [{problem_id}]({link}) | [{problemName}]({link}) |\n"
+            lines.append(line)
+        lines.append('\n')
+
+    with open(save_file, 'w') as f:
+        f.writelines(lines)
         f.close()
 
-    pick_data = list()
-    for key, data in database.items():
-        if 1 <= data.get('problemLevel') <= 15:
-            line = f"{key}${data.get('problemLevel')}\n"
-            pick_data.append(line)
-
-    with open('./scripts/pick_data.in', 'w') as f:
-        f.writelines(pick_data)
-        f.close()
-
-    return pick_data
-
-def update():
-    data = None
-    with open('./scripts/pick_data.in', 'r') as f:
-        data = f.readlines()
-        f.close()
-
-    if len(data) <= 30:
-        data = reset()
-
-    problem_database = None
-    with open('./scripts/database.json', 'r') as f:
-        problem_database = json.load(f)
-        f.close()
-
-    newdata = list()
-    for line in data:
-        problemId, problemLevel = line.split('$')
-        problemInfo = api.request(problemId)
-        problemLevel = problemInfo["problemLevel"]
-        newdata.append(f"{problemId}${problemLevel}\n")
-
-    with open('./scripts/pick_data.in', 'w') as f:
-        f.writelines(newdata)
-        f.close()
-
-def make_table():
-    data = dict()
-    with open('./scripts/picked.json', 'r') as f:
-        data = json.load(f)
-        f.close()
-
-    saveline = []
-    keys = data.keys()
-    for key in keys:
-        saveline.append(f"## {key} \n")
-        saveline.append('\n')
-        saveline.append("| 난이도 | 번호 | 문제 이름 |\n")
-        saveline.append("|:------:|:----:|:---------:|\n")
-        for problem in data[key]:
-            problemInfo = api.request(problem)
-            problemName = problemInfo.get('problemName')
-            problemLevel = problemInfo.get('problemLevel')
-            imageurl = f"<img height=\"25px\" width=\"25px\" src=\"https://static.solved.ac/tier_small/{problemLevel}.svg\"/>"
-            line = f"| {imageurl} | [{problem}](https://www.acmicpc.net/problem/{problem}) | [{problemName}](https://www.acmicpc.net/problem/{problem}) |\n"
-            saveline.append(line)
-
-        saveline.append('\n')
-
-    with open('./picked.md', 'w') as f:
-        f.writelines(saveline)
-        f.close()
 
 def pick():
-    os.system('g++ -std=c++17 ./scripts/pick_problem.cpp -o ./scripts/pick.problem')
-
-    data = None
-    with open('./scripts/pick_data.in', 'r') as f:
-        data = f.readlines()
-        f.close()
-
-    os.system('./scripts/pick.problem < ./scripts/pick_data.in > ./scripts/today_problem.out')
-    with open("./scripts/today_problem.out", "r") as f:
-        ret = [ _.strip() + '$' for _ in f.readlines() ]
-        f.close()
-
-    remain = list()
-    picked = list()
-    for v in ret:
-        for idx, line in enumerate(data):
-            if line.startswith(v):
-                picked.append(line)
-                del data[idx]
-                break
-
-    remain = data
-
     picked_json = dict()
     with open('./scripts/picked.json', 'r') as f:
         picked_json = json.load(f)
         f.close()
 
-    timeformat = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
-    timeformat = f"{timeformat.strftime('%Y/%m/%d')}"
-    new_data = dict()
-    new_data[timeformat] = list()
-    for problem in picked:
-        problemId, problemLevel = problem.strip().split('$')
-        new_data[timeformat].append(problemId)
+    timeformat = get_today_date()
 
+    today_problems: list = Communication.get_today_problem()
+    new_data = {}
+    new_data[timeformat] = today_problems
     new_data.update(picked_json)
 
     with open('./scripts/picked.json', 'w') as f:
         f.write(json.dumps(new_data, indent=4, ensure_ascii=False))
         f.close()
 
-    with open('./scripts/pick_data.in', 'w') as f:
-        f.writelines(remain)
-        f.close()
+    return picked_json
+
+
+def main():
+    data = pick()
+    today = get_today_date()
+    make_table({today : data[today]}, './picked.md')
+    make_table(data, './picked_legacy.md')
+
 
 if __name__ == "__main__":
-    config = None
-    with open('./scripts/config.json', 'r') as f:
-        config = json.load(f)
-        f.close()
-
-    api = SolvedAPI(config.get("API"))
-    
-    update()
-    pick()
-    make_table()
+    main()
